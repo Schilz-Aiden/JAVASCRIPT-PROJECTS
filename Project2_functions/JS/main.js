b3864dc6d5636dd25f6f5c9fb8a3158c891ff677
SchilzAiden <aidenschilz5@gmail.com>
2025-03-03 11:03:51 -0700

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var sentence = "I am learning how to connect"; //Using += allows to connect multiple sentences together.
+    sentence += " two sentences together";
+    sentence += " by using Concatenates!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
