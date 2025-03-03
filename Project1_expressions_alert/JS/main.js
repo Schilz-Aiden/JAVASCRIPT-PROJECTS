ee9f6ca262ef26df87364e79292b2a863b726256
SchilzAiden <aidenschilz5@gmail.com>
2025-03-03 10:32:31 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+var CompleteSent = Sent1 + Sent2 //Combining both variables to complete the
+                                //sentence together.
+
+document.write(CompleteSent); //The final output of third variable.
