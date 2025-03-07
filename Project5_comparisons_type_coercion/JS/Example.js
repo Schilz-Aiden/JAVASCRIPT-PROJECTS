7cd4dbe730ff82c3b3344af486a7bfbe466bae16
SchilzAiden <aidenschilz5@gmail.com>
2025-03-07 11:51:25 -0700

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/Example.js
@@ -0,0 +1,3 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = isNaN("This is a string"); //Function to prove that the turn is not a number.
+}
---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,5 @@
+console.log(5+7);
+console.log(5 === 5);
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10); //Function with a double negative to show if the equation is true or false
+}
